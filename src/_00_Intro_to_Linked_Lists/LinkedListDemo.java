43f518191999695667205bc593beca636a31483a
league <57720048+richardson-liou@users.noreply.github.com>
2023-04-09 11:28:11 -0700

    onLinkedList

--- src/_00_Intro_to_Linked_Lists/LinkedListDemo.java
+++ src/_00_Intro_to_Linked_Lists/LinkedListDemo.java
@@ -31,6 +31,14 @@ public class LinkedListDemo {
          * String nodes.
          * 
          */
+    	
+LinkedList<Integer> ll = new LinkedList<Integer>();
+
+ll.add(3);
+ll.add(4);
+ll.add(5);
+ll.add(6);
+ll.print();
 
     }
 
